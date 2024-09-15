aa6ade803f30ed010791a5e1051277437d53d7aa
architaga <124569000+architaga@users.noreply.github.com>
2024-09-15 23:21:51 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class StudentGradeCalculator{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of subject:");
+        int numberOfSubjects=sc.nextInt();
+        int TotalMarks=0;
+        for(int i=1;i<=numberOfSubjects;i++){
+            System.out.print("Enter marks for subject "+i+" (out of 100): ");
+            int marks=sc.nextInt();
+            TotalMarks += marks;
+        }
+        double averagePercentage=(TotalMarks*100.0)/(numberOfSubjects*100);
+        String grade;
+        if(averagePercentage>=90){
+            grade="A";
+        }else if(averagePercentage>=80){
+            grade="B";
+
+        }else if(averagePercentage>=70){
+            grade="C";
+        }else if(averagePercentage>=60){
+            grade="D";
+        }else{
+            grade="F";
+        }
+        System.out.println("\nResults:");
+        System.out.println("Total marks: "+TotalMarks);
+        System.out.println("Average percentage: "+averagePercentage+"%");
+        System.out.println("grade: "+grade);
+
+    }
+}
